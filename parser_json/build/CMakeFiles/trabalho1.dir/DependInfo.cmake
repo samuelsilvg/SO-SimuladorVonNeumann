51784445301d500ccffe1e08fb8ab720de29a431
radsfer <rafael.ferreira11.98@gmail.com>
2025-10-02 13:36:05 +0000

    Parser adicionado a main ainda n integrado

---
+++ parser_json/build/CMakeFiles/trabalho1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/eduardo/Downloads/Trabalho SO 1/Trabalho SO 1/Parser-Json---MIPS/src/MainMemory.cpp" "CMakeFiles/trabalho1.dir/src/MainMemory.cpp.o" "gcc" "CMakeFiles/trabalho1.dir/src/MainMemory.cpp.o.d"
+  "/home/eduardo/Downloads/Trabalho SO 1/Trabalho SO 1/Parser-Json---MIPS/src/main.cpp" "CMakeFiles/trabalho1.dir/src/main.cpp.o" "gcc" "CMakeFiles/trabalho1.dir/src/main.cpp.o.d"
+  "/home/eduardo/Downloads/Trabalho SO 1/Trabalho SO 1/Parser-Json---MIPS/src/parser_json.cpp" "CMakeFiles/trabalho1.dir/src/parser_json.cpp.o" "gcc" "CMakeFiles/trabalho1.dir/src/parser_json.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
